2a16c20f650b356c99d0afd4a77f0d2e73f72615
juzten <juzten@gmail.com>
2015-02-07 21:34:27 -0500

    updated port info

--- app/app.py
+++ app/app.py
@@ -20,4 +20,5 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', debug=True, port=port)
