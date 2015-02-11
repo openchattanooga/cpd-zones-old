0f7afa1005d733c4856b11d2ed3a11e55462dca9
Sean Brewer <seabre986@gmail.com>
2015-02-11 18:57:00 -0500

    Add Postgres connection and migrations.

--- app/app.py
+++ app/app.py
@@ -4,11 +4,23 @@ import urllib
 import json
 import random
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.script import Manager
+from flask.ext.migrate import Migrate, MigrateCommand
 from flask import render_template
 from forms import SearchForm
 
 app = Flask(__name__)
 app.secret_key = 'some_secret_key'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
 
 
 @app.route("/", methods=['GET', 'POST'])
@@ -21,5 +33,4 @@ def index():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', debug=True, port=port)
+    manager.run()
