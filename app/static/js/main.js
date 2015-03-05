9a4253e058ff34b5226f54604a55925e9b85e340
Sean Brewer <seabre986@gmail.com>
2015-03-04 19:21:20 -0500

    Replace local tile URL with Chattanooga Public Library map tiles on GitHub.

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -11,7 +11,7 @@ var map = L.map('map', {
   zoom: 12
 });
 
-L.tileLayer('static/tiles/{z}/{x}/{y}.png', {
+L.tileLayer('http://chattanoogapubliclibrary.github.io/cpd-zones-tiles/tiles/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Zone data &copy; City of Chattanooga, Imagery © <a href="http://mapbox.com">Mapbox</a>'}).addTo(map);
 
 r
