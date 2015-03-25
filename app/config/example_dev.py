c58b8e22b54d0b554e481a3679183f8f26555ef2
juzten <juzten@gmail.com>
2015-03-25 18:38:54 -0400

    instructions for config

--- app/config/example_dev.py
+++ app/config/example_dev.py
@@ -3,5 +3,11 @@
 import os
 basedir = os.path.abspath(os.path.join(os.path.dirname( __file__ ), os.pardir))
 
+"""Notes.
+
+1. Copy this file and rename it to dev.py
+2. Update the below variables to match your machine
+"""
+
 DATABASE_URL = 'postgresql:///cpd-zones'
 SECRET_KEY = 'awesome_sauce'
