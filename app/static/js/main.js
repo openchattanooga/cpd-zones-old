265fc05c597db7cd8ecbf08d1c1be20098aa83c9
Sean Brewer <seabre986@gmail.com>
2015-03-26 01:09:07 -0400

    Get rid of stray character in main.js

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -13,5 +13,3 @@ var map = L.map('map', {
 
 L.tileLayer('//chattanoogapubliclibrary.github.io/cpd-zones-tiles/tiles/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Zone data &copy; City of Chattanooga, Imagery © <a href="http://mapbox.com">Mapbox</a>'}).addTo(map);
-
-r
